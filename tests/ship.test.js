8d7820938639b88f9a05aeea5638bdd325e0afd4
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-09-19 12:26:17 +1000

    Create and test ship class

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -7,4 +7,46 @@ describe("Ship", () => {
 		expect(ship.hits).toBe(0);
 		expect(ship.sunk).toBe(false);
 	});
+
+	test("hit() should increase the hits count", () => {
+		const ship = new Ship(3);
+		ship.hit();
+		expect(ship.hits).toBe(1);
+		expect(ship.sunk).toBe(false);
+	});
+
+	test("hit() should not increase hits count if the ship is already sunk", () => {
+		const ship = new Ship(3);
+		ship.hit();
+		ship.hit();
+		ship.hit();
+		expect(ship.hits).toBe(3); // Ship is now sunk
+		ship.hit(); // This should not increase hits further
+		expect(ship.hits).toBe(3); // Hits should remain 3
+	});
+
+	test("checkIfSunk() should return true when the ship is sunk", () => {
+		const ship = new Ship(2);
+		ship.hit();
+		ship.hit(); // Ship is now sunk
+		expect(ship.isSunk()).toBe(true);
+	});
+
+	test("checkIfSunk() should return false when the ship is not sunk", () => {
+		const ship = new Ship(2);
+		ship.hit();
+		expect(ship.isSunk()).toBe(false);
+	});
+
+	test("Creating a Ship with a Length of 0 should throw an error", () => {
+		expect(() => new Ship(0)).toThrowError(
+			"Ship length must be greater than zero."
+		);
+	});
+
+	test("Creating a Ship with a Negative Length should throw an error", () => {
+		expect(() => new Ship(-2)).toThrowError(
+			"Ship length must be greater than zero."
+		);
+	});
 });
